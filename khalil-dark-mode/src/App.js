66bd53f26456083d3bc12f1bdb46c6583a440bdb
kwarnerredhat <kwarner@redhat.com>
2023-08-22 15:06:15 -0400

    My finished dark mode

--- khalil-dark-mode/src/App.js
+++ khalil-dark-mode/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; 
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={`app ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      <header>
+        <h1>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</h1>
+        <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
       </header>
+      <main>
+        {'Welcome to my dark mode'}
+      </main>
     </div>
   );
 }
