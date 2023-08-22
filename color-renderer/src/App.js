49724354a450428496cd2b933c758ccf29ef488c
kwarnerredhat <kwarner@redhat.com>
2023-08-22 13:00:06 -0400

    My finished renderer

--- color-renderer/src/App.js
+++ color-renderer/src/App.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import ColorRenderer from './ColorRenderer';
- ColorRenderer = () => {
-  const [selectedColor, setSelectedColor] = useState('#FF5733'); 
 
-  const handleColorChange = (color) => {
-    setSelectedColor(color);
-  };
+const ColorRenderer = () => {
+  const [selectedColor, setSelectedColor] = useState('#FF5733'); // Default color: Orange
 
   return (
     <div>
-      <h1>Simple Color Renderer</h1>
+      <h1>Khalil Color Renderer</h1>
       <div style={{ backgroundColor: selectedColor, width: '100px', height: '100px' }}></div>
-      <input type="color" value={selectedColor} onChange={(e) => handleColorChange(e.target.value)} />
+      <input
+        type="color"
+        value={selectedColor}
+        onChange={(e) => setSelectedColor(e.target.value)}
+      />
       <p>Selected Color: {selectedColor}</p>
     </div>
   );
