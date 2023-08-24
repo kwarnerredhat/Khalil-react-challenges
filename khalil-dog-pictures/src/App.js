31a09e5c6cdaf80999d01acb5580edc75d56220a
kwarnerredhat <kwarner@redhat.com>
2023-08-24 11:41:09 -0400

    My finished dog fetch

--- khalil-dog-pictures/src/App.js
+++ khalil-dog-pictures/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const DogComponent = () => {
+  const [dogImage, setDogImage] = useState('');
+
+  useEffect(() => {
+    fetchDogImage();
+  }, []);
+
+  const fetchDogImage = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      setDogImage(data.message);
+    } catch (error) {
+      console.error('Error fetching dog image:', error);
+    }
+  };
+
+  const handleFetchNewDog = () => {
+    fetchDogImage();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Dog Image</h1>
+      <img src={dogImage} alt="Random Dog" />
+      <button onClick={handleFetchNewDog}>Fetch New Dog</button>
     </div>
   );
-}
+};
 
-export default App;
+export default DogComponent;
+
