81c9619594aeef8fb4a50ec4a08ddafa85146484
kwarnerredhat <kwarner@redhat.com>
2023-08-25 13:03:17 -0400

    .

--- khalil-dog-pictures/src/App.js
+++ khalil-dog-pictures/src/App.js
@@ -8,7 +8,7 @@ const DogComponent = () => {
   }, []);
 
   const fetchDogImage = async () => {
-    try {
+    try{
       const response = await fetch('https://dog.ceo/api/breeds/image/random');
       const data = await response.json();
       setDogImage(data.message);
